b9c453ac0e09335d9354bf6b7eba5d46a4aad99f
zidankhan86 <zidankhan718@gmail.com>
2023-02-15 14:45:51 +0600

    icon

--- resources/views/backend/fixed/sidebar.blade.php
+++ resources/views/backend/fixed/sidebar.blade.php
@@ -9,50 +9,50 @@
       </li>
       <li class="nav-item">
         <a class="nav-link" href="{{ route('shop.list') }}">
-          <i class="menu-icon mdi mdi-file-document"></i>
+          <i class="menu-icon ti-home" ></i>
           <span class="menu-title">MY SHOP</span>
         </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="{{route ('shop.form') }}">
-          <i class="menu-icon mdi mdi-file-document"></i>
+          <i class="menu-icon ti-home"></i>
           <span class="menu-title">SHOPS</span>
         </a>
       </li>
         <li class="nav-item">
             <a class="nav-link" href="{{route ('product.form') }}">
-              <i class="menu-icon mdi mdi-file-document"></i>
+              <i class="menu-icon ti-package"></i>
               <span class="menu-title">PRODUCTS</span>
             </a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="{{route ('category.form') }}">
-              <i class="menu-icon mdi mdi-file-document"></i>
+              <i class="menu-icon  ti-layout-media-overlay"></i>
               <span class="menu-title">CATEGORY</span>
             </a>
           </li>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="{{route ('order.list') }}">
-          <i class="menu-icon mdi mdi-file-document"></i>
+          <i class="menu-icon ti-bag"></i>
           <span class="menu-title">ORDERS</span>
         </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="{{ route('customer.add') }}">
-          <i class="menu-icon mdi mdi-file-document"></i>
+          <i class="menu-icon ti-user"></i>
           <span class="menu-title">CUSTOMERS</span>
         </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="">
-          <i class="menu-icon mdi mdi-file-document"></i>
+          <i class="menu-icon ti-truck"></i>
           <span class="menu-title">SHIPPINGS</span>
         </a>
       </li>
       <li class="nav-item">
         <a class="nav-link" href="">
-          <i class="menu-icon mdi mdi-file-document"></i>
+          <i class="menu-icon ti-loop`"></i>
           <span class="menu-title">REFOUNDS</span>
         </a>
       </li>
