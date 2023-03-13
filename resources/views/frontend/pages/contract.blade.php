03ec17e231102da8865c4c88b552b462297bd78f
zidankhan86 <zidankhan718@gmail.com>
2023-03-13 18:09:06 +0600

    Contract form

--- resources/views/frontend/pages/contract.blade.php
+++ resources/views/frontend/pages/contract.blade.php
@@ -26,31 +26,31 @@
                 </div>
             </div>
             <div class="col-lg-7 col-md-12 wow fadeInUp" data-wow-delay="0.5s">
-                <p class="mb-4">The contact form is currently inactive. Get a functional and working contact form with Ajax & PHP in a few minutes. Just copy and paste the files, add a little code and you're done. <a href="https://htmlcodex.com/contact-form">Download Now</a>.</p>
+                <p class="mb-4">Please Write If You Faceing Any Problem With Our Site . Share How Can Grow. How Can We Improve  .</p>
                 <form>
                     <div class="row g-3">
                         <div class="col-md-6">
                             <div class="form-floating">
-                                <input type="text" class="form-control" id="name" placeholder="Your Name">
-                                <label for="name">Your Name</label>
+                                <input type="text" class="form-control" name="name" id="name" placeholder="Your Name">
+                                <label for="name">Enter Your Name</label>
                             </div>
                         </div>
                         <div class="col-md-6">
                             <div class="form-floating">
-                                <input type="email" class="form-control" id="email" placeholder="Your Email">
-                                <label for="email">Your Email</label>
+                                <input type="email" class="form-control" name="email" id="email" placeholder="Your Email">
+                                <label for="email">Enter Your Email</label>
                             </div>
                         </div>
                         <div class="col-12">
                             <div class="form-floating">
-                                <input type="text" class="form-control" id="subject" placeholder="Subject">
-                                <label for="subject">Subject</label>
+                                <input type="text" class="form-control" name="subject"  id="subject" placeholder="Subject">
+                                <label for="subject">Enter Subject</label>
                             </div>
                         </div>
                         <div class="col-12">
                             <div class="form-floating">
-                                <textarea class="form-control" placeholder="Leave a message here" id="message" style="height: 200px"></textarea>
-                                <label for="message">Message</label>
+                                <textarea class="form-control" name="message" placeholder="Leave a message here" id="message" style="height: 200px"></textarea>
+                                <label for="message">Write Message</label>
                             </div>
                         </div>
                         <div class="col-12">
