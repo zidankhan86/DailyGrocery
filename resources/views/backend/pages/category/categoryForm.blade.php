52ba006fe9ec7416124c1d053f1a33f459a09f1b
zidankhan86 <zidankhan718@gmail.com>
2023-03-31 23:26:37 +0600

    Category form client side validation

--- resources/views/backend/pages/category/categoryForm.blade.php
+++ resources/views/backend/pages/category/categoryForm.blade.php
@@ -8,7 +8,7 @@
         @csrf
         <div class="form-group">
           <label for="exampleInputName1">Name</label>
-          <input type="text" class="form-control" name="name" id="exampleInputName1" placeholder="Name">
+          <input type="text" class="form-control" name="name" required id="exampleInputName1" placeholder="Name">
         </div>
         <div class="form-group">
           <label for="exampleInputEmail3">Details</label>
