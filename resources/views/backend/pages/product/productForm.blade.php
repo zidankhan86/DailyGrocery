29ecf07a7442386986972f912539b008cdc0d021
zidankhan86 <zidankhan718@gmail.com>
2023-03-31 23:29:53 +0600

    Product form client side validation

--- resources/views/backend/pages/product/productForm.blade.php
+++ resources/views/backend/pages/product/productForm.blade.php
@@ -9,11 +9,11 @@
         @csrf
         <div class="form-group">
           <label for="exampleInputName1">Product Name</label>
-          <input type="text" class="form-control" name="product_name" id="exampleInputName1" placeholder="Product Name">
+          <input type="text" class="form-control" name="product_name" required id="exampleInputName1" placeholder="Product Name">
         </div>
         <div class="form-group">
             <label for="exampleInputName1">Category Name</label>
-            <select class="form-control" name="category_id" id="">
+            <select class="form-control" name="category_id" required id="">
 
                 @foreach ($categories as $category)
 
@@ -24,27 +24,27 @@
           </div>
         <div class="form-group">
           <label for="exampleInputEmail3">Shop Name</label>
-          <input type="text" class="form-control" name="shop_name" id="exampleInputEmail3" placeholder="Shop Name">
+          <input type="text" class="form-control" name="shop_name" required id="exampleInputEmail3" placeholder="Shop Name">
         </div>
         <div class="form-group">
           <label for="exampleInputPassword4">Product Image</label>
-          <input type="file" class="form-control" name="image" id="exampleInputPassword4" placeholder="Shop Image">
+          <input type="file" class="form-control" name="image" required id="exampleInputPassword4" placeholder="Shop Image">
         </div>
         <div class="form-group">
           <label for="exampleInputCity1">Price</label>
-          <input type="number" class="form-control" name="price" id="exampleInputCity1" placeholder="Price">
+          <input type="number" class="form-control" name="price" required id="exampleInputCity1" placeholder="Price">
         </div>
         <div class="form-group">
             <label for="exampleInputPassword4">Quantity</label>
-            <input type="number" class="form-control" name="quantity" id="exampleInputPassword4" placeholder="Quantity">
+            <input type="number" class="form-control" name="quantity" required id="exampleInputPassword4" placeholder="Quantity">
           </div>
         <div class="form-group">
           <label for="exampleTextarea1">Product Details</label>
-          <textarea class="form-control" name="details" id="exampleTextarea1" rows="4" placeholder="Details"></textarea>
+          <textarea class="form-control" name="details" required id="exampleTextarea1" rows="4" placeholder="Details"></textarea>
         </div>
         <div class="form-group">
             <label for="exampleInputPassword4">Product Status</label>
-            <input type="text" class="form-control" name="status"  id="exampleInputPassword4" placeholder="Status">
+            <input type="text" class="form-control" name="status" required  id="exampleInputPassword4" placeholder="Status">
           </div>
         <button type="submit" class="btn btn-primary me-2">Submit</button>
         <button class="btn btn-light">Cancel</button>
