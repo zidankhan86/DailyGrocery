1706736daa886415f9b9a335892e0aada5fb28d7
zidankhan86 <zidankhan718@gmail.com>
2023-03-31 23:25:28 +0600

    Brand form client side validation

--- resources/views/backend/pages/brand/brandForm.blade.php
+++ resources/views/backend/pages/brand/brandForm.blade.php
@@ -8,12 +8,12 @@
         @csrf
         <div class="form-group">
           <label for="exampleInputName1">Brand Name</label>
-          <input type="text" class="form-control" name="name" id="exampleInputName1" placeholder="Name">
+          <input type="text" class="form-control" name="name" id="exampleInputName1" required placeholder="Name">
         </div>
 
         <div class="form-group">
             <label for="exampleInputName1">Brand logo</label>
-            <input type="file" class="form-control" name="logo" id="exampleInputName1" placeholder="Name">
+            <input type="file" class="form-control" name="logo" required id="exampleInputName1" placeholder="Name">
           </div>
 
         <button type="submit" class="btn btn-primary me-2">Submit</button>
