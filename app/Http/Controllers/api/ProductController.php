57b7aa47bbab3b58cba5b2ccd9fa206f3f205aa8
zidankhan86 <zidankhan718@gmail.com>
2023-05-14 11:48:17 +0600

    API get request

---
+++ app/Http/Controllers/api/ProductController.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Blog;
+use App\Models\Category;
+use App\Models\Products;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    public function product(){
+       $product = Products::all();
+       return response()->json($product);
+    }
+
+    public function category(){
+        $category = Category::all();
+        return response()->json($category);
+    }
+
+    public function blog(){
+        $blog = Blog::all();
+        return response()->json( $blog);
+    }
+
+
+}
