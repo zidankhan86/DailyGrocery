3d465b636b34685809b044cdddc3a04690ab82e2
zidankhan86 <zidankhan718@gmail.com>
2023-05-30 00:51:06 +0600

    Registration Form

--- resources/views/backend/pages/loginRegistration/registration.blade.php
+++ resources/views/backend/pages/loginRegistration/registration.blade.php
@@ -1,101 +1,51 @@
 
-  <!DOCTYPE html>
+<!DOCTYPE html>
 <html lang="en">
 
+
+<!------ Include the above in your HEAD tag ---------->
 <head>
-  <!-- Required meta tags -->
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <title>Star Admin2 </title>
-  <!-- plugins:css -->
-  <link rel="stylesheet" href="../../vendors/feather/feather.css">
-  <link rel="stylesheet" href="../../vendors/mdi/css/materialdesignicons.min.css">
-  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
-  <link rel="stylesheet" href="../../vendors/typicons/typicons.css">
-  <link rel="stylesheet" href="../../vendors/simple-line-icons/css/simple-line-icons.css">
-  <link rel="stylesheet" href="../../vendors/css/vendor.bundle.base.css">
-  <!-- endinject -->
-  <!-- Plugin css for this page -->
-  <link rel="stylesheet" href="../../vendors/select2/select2.min.css">
-  <link rel="stylesheet" href="../../vendors/select2-bootstrap-theme/select2-bootstrap.min.css">
-  <!-- End plugin css for this page -->
-  <!-- inject:css -->
-  <link rel="stylesheet" href="../../css/vertical-layout-light/style.css">
-  <!-- endinject -->
-  <link rel="shortcut icon" href="../../images/favicon.png" />
+    <br><br><br><link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+
 </head>
 
 <body>
-  <div class="container-scroller">
 
-    <!-- partial -->
-    <div class="container-fluid page-body-wrapper mx-auto">
-      <!-- partial:../../partials/_settings-panel.html -->
+<form action="{{route ('create.registration') }}" method="post" enctype="multipart/form-data">
+    @csrf
+<div style="background-color:#41aaa2; height:400px; width:400px; border-radius:30px; text-align:center;" class="col-md-offset-4">
+<h1 style="text-align:center; padding-top:30px; color:#ffffff;">Sign Up</h1>
 
-      <!-- partial -->
-      <div class="main-panel mx-auto">
-        <div class="content-wrapper">
-          <div class="row">
-            <div class="col-12 grid-margin stretch-card">
-              <div class="card">
-                <div class="card-body">
-                  <h4 class="card-title">Create DailyGrocery Account</h4>
+<div class="col-sm-12" style="padding-top:10px;">
+<input type="text" class="form-control"  placeholder=" Name"  name="name" style="border-radius:15px;">
+</div>
+<div class="col-sm-12" style="padding-top:10px;">
+<input type="email" class="form-control"  placeholder="Email" name="email" style="border-radius:15px;">
+</div>
+<div class="col-sm-12" style="padding-top:10px;">
+<input type="text" class="form-control" id="email" placeholder="Enter Address" name="address" style="border-radius:15px;">
+</div>
+<div class="col-sm-12" style="padding-top:10px;">
+<input type="text" class="form-control" id="mobileno" placeholder="Enter Mobile No." name="phone" style="border-radius:15px;">
+</div>
+<div class="col-sm-12" style="padding-top:10px;">
+<input type="password" class="form-control" id="password" placeholder="Type your Password" name="password" style="border-radius:15px;">
+</div>
 
-                 <form class="forms-sample" action="{{route ('create.registration') }}" method="post">
-        @csrf
-        <div class="form-group">
-          <label for="exampleInputUsername1">Your Name</label>
-          <input type="text" name="name" class="form-control" id="exampleInputUsername1" placeholder="Username">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputEmail1">Email address</label>
-          <input type="email" name="email" class="form-control" id="exampleInputEmail1" placeholder="Email">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputPassword1">Address</label>
-          <input type="text" name="address" class="form-control" id="exampleInputPassword1" placeholder="Enter Address">
-        </div>
-        <div class="form-group">
-          <label for="exampleInputConfirmPassword1">Phone</label>
-          <input type="tel" name="phone" class="form-control" id="exampleInputConfirmPassword1" placeholder="Enter Phone">
-        </div>
-        <div class="form-group">
-            <label for="exampleInputConfirmPassword1">Password</label>
-            <input type="password" name="password" class="form-control" id="exampleInputConfirmPassword1" placeholder="Password">
-          </div>
-        <div class="form-check form-check-flat form-check-primary">
-          <label class="form-check-label">
-            <input type="checkbox" class="form-check-input">
-            Remember me
-          </label>
-        </div>
-        <button type="submit" class="btn btn-primary me-2">Submit</button>
-        <button class="btn btn-light">Cancel</button>
 
-        <!-- content-wrapper ends -->
-        <!-- partial:../../partials/_footer.html -->
-  <!-- container-scroller -->
-  <!-- plugins:js -->
-  <script src="../../vendors/js/vendor.bundle.base.js"></script>
-  <!-- endinject -->
-  <!-- Plugin js for this page -->
-  <script src="../../vendors/typeahead.js/typeahead.bundle.min.js"></script>
-  <script src="../../vendors/select2/select2.min.js"></script>
-  <script src="../../vendors/bootstrap-datepicker/bootstrap-datepicker.min.js"></script>
-  <!-- End plugin js for this page -->
-  <!-- inject:js -->
-  <script src="../../js/off-canvas.js"></script>
-  <script src="../../js/hoverable-collapse.js"></script>
-  <script src="../../js/template.js"></script>
-  <script src="../../js/settings.js"></script>
-  <script src="../../js/todolist.js"></script>
-  <!-- endinject -->
-  <!-- Custom js for this page-->
-  <script src="../../js/file-upload.js"></script>
-  <script src="../../js/typeahead.js"></script>
-  <script src="../../js/select2.js"></script>
-  <!-- End custom js for this page-->
+<button class="col-md-4 col-md-offset-4 btn" style="background-color:#ffffff; color:#626a69; font-size:20px; margin-top:10px;" type="submit"> Submit
+</button>
+</div>
+</form>
+
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </body>
 
 </html>
+
+
+
+
+
 
